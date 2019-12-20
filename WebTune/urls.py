cf28bbbe36b1f455f71b090fdf9e47c4d391df52
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-12-21 00:12:07 +0530

    generic view over functions

--- WebTune/urls.py
+++ WebTune/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = 'WebTune'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),  # landing page
-    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),  # details of the songs
-    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),  # favourite function
+    url(r'^$', views.IndexView.as_view(), name='index'),  # landing page
+    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='details'),  # details of the songs
+    # url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),  # favourite function
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- WebTune/views.py
+++ WebTune/views.py
@@ -1,31 +1,32 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
+from django.views import generic
 
 
-def index(request):
-    all_albums = Album.objects.all()
-    context = {
-        'all_albums': all_albums,
-    }
-    return render(request, 'WebTune/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'WebTune/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
 
 
-def details(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)  # replaces entire try and catch
-    return render(request, 'WebTune/details.html', {'album': album})  # another way of doing the dictionary work
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'WebTune/details.html'
 
 
-def favourite(request, album_id):
-    album = get_object_or_404(Album, pk=album_id)  # obtaining album
-    # print(request.POST['song'])
-    try:
-        selected_song = album.song_set.get(pk=request.POST['song'])
-        selected_song.is_favourite = True
-        selected_song.save()
-        return render(request, 'WebTune/details.html', {'album': album})  # adds songs to favourite list
-
-    except (KeyError, Song.DoesNotExist):
-        return render(request, 'WebTune/details.html', {
-            'album': album,
-            'error_message': "You did not select a valid song",
-        })
+# def favourite(request, album_id):
+#     album = get_object_or_404(Album, pk=album_id)  # obtaining album
+#     # print(request.POST['song'])
+#     try:
+#         selected_song = album.song_set.get(pk=request.POST['song'])
+#         selected_song.is_favourite = True
+#         selected_song.save()
+#         return render(request, 'WebTune/details.html', {'album': album})  # adds songs to favourite list
+#
+#     except (KeyError, Song.DoesNotExist):
+#         return render(request, 'WebTune/details.html', {
+#             'album': album,
+#             'error_message': "You did not select a valid song",
+#         })
