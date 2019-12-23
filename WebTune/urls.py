4b1c0cc22f3cba39d73910f59e9c479d3605627c
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-12-23 22:30:21 +0530

    Deleted logos folder

--- WebTune/urls.py
+++ WebTune/urls.py
@@ -6,7 +6,7 @@ app_name = 'WebTune'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),  # landing page
-    url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),  # details of the songs
+    url(r'^album/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),  # details of the songs
     url(r'^album/add/$', views.AlbumCreateView.as_view(), name='album-add'),  # album addition view
 ]
 
