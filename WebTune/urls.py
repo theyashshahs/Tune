ed8aaa3de10325d3f1088c24106db724d574ac5a
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-12-26 22:30:31 +0530

    changes in url structure

--- WebTune/urls.py
+++ WebTune/urls.py
@@ -7,8 +7,7 @@ app_name = 'WebTune'
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),  # landing page
     url(r'^album/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),  # details of the songs
-    url(r'^album/add/$', views.AlbumCreateView.as_view(), name='album-add'),  # album addition view
+    url(r'^album/add/', views.AlbumCreateView.as_view(), name='album-add'),  # album addition view
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-
