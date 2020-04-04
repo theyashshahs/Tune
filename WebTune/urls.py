d534f3ca84ac4a0efa4eb618b8aeb55fe08d53a6
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-04-04 16:12:09 +0530

    added edit button in albums

--- WebTune/urls.py
+++ WebTune/urls.py
@@ -8,11 +8,14 @@ app_name = 'WebTune'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),  # landing page
-    url(r'^album/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),  # details of the songs
-    url(r'^album/add/$', views.AlbumCreateView.as_view(), name='album-add'),  # album addition view
-    url(r'^album/update/(?P<pk>[0-9]+)/', views.AlbumUpdateView.as_view(), name='album-update'),
-    url(r'^album/(?P<pk>[0-9]+)/delete/', views.AlbumDeleteView.as_view(), name='album-delete'),
+    url(r'^album/(?P<pk>[0-9]+)/$', views.DetailView.as_view(),
+        name='detail'),  # details of the songs
+    url(r'^album/add/$', views.AlbumCreateView.as_view(),
+        name='album-add'),  # album addition view
+    url(r'^album/update/(?P<pk>[0-9]+)/',
+        views.AlbumUpdateView.as_view(), name='album-update'),
+    url(r'^album/(?P<pk>[0-9]+)/delete/',
+        views.AlbumDeleteView.as_view(), name='album-delete'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
