f9c1987c624b029337182ef3c4f21eb519a5af90
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-04-05 01:06:53 +0530

    login added

--- WebTune/views.py
+++ WebTune/views.py
@@ -1,7 +1,11 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from django.views import generic
+from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from WebTune.models import Album, Song
+from WebTune.forms import UserForm
 
 
 class IndexView(generic.ListView):
@@ -11,6 +15,31 @@ class IndexView(generic.ListView):
     def get_queryset(self):
         return Album.objects.all()
 
+
+class UserView(View):
+    form_class = UserForm
+    template_name = 'WebTune/register.html'
+
+    def get(self, request):
+        form = self.form_class(None)
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user.set_password(password)
+            user.save()
+            user = authenticate(username=username, password=password)
+
+            if user.is_active:
+                login(request, user)
+                return redirect('WebTune:index')
+
+        return render(request, self.template_name, {'form': form})
 
 class AlbumDetailView(generic.DetailView):
     model = Album
