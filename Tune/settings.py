23bf0afcaec7682b4158c9c54bac15327e892aaf
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-04-11 17:38:21 +0530

    serving static files through s3 bucket :cloud:

--- Tune/settings.py
+++ Tune/settings.py
@@ -135,6 +135,13 @@ AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+
+# AWS S3 bucket static files serving 
+AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
