50ee420e289894e603fbff2375622301ae2d0d4a
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-10 23:41:52 +0530

    user app created

--- Tune/settings.py
+++ Tune/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'WebTune.apps.WebtuneConfig',
+    'users.apps.UsersConfig',
     'storages'
 ]
 
--- WebTune/models.py
+++ WebTune/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Album(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
-        return self.album_title + ' => ' + self.artist
+        return self.album_title + ' - ' + self.artist
 
 
 class Song(models.Model):
