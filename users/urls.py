504f6fa22c75358a429c1a680fcd7e85f0becc63
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-11 19:26:45 +0530

    logout view added

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ app_name = 'users'
 urlpatterns = [
     url(r'^register/', views.registration, name='register'),
     url(r'^login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/login.html'), name='logout')
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout')
 ]
