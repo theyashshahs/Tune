2b2ceab3f270670a71adc3b027dab305afaa8bb3
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-13 17:32:38 +0530

    image size reducing function added

--- music/models.py
+++ music/models.py
@@ -1,17 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 
 class Album(models.Model):
-    user = models.ForeignKey(User, default=None, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     artist = models.CharField(max_length=250)
     album_title = models.CharField(max_length=500)
     genre = models.CharField(max_length=100)
     album_logo = models.ImageField(upload_to='images/', blank=True)
 
     def __str__(self):
-        return self.album_title + ' - ' + self.artist
+        return f'{self.user} {self.album_title}'
 
+    # def save(self, *args, **kwargs):
+    #     super().save(*args, **kwargs)
+
+    #     img = Image.open(self.album_logo.path)
+
+    #     if img.height > 300 or img.width > 300:
+    #         output_size = (300, 300)
+    #         img.thumbnail(output_size)
+    #         img.save(self.album_logo.path)
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
