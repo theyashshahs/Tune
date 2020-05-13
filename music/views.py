1cd278a21da1106efedfacff6e02ccf1ea31f9dd
yashshah2820 <201851150@iiitvadodara.ac.in>
2020-05-14 00:20:29 +0530

    user permission added for update, delete views

--- music/views.py
+++ music/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.urls import reverse_lazy
 from music.models import Album, Song
 
@@ -13,38 +14,61 @@ class IndexListView(generic.ListView):
     template_name = 'music/index.html'
     context_object_name = 'all_albums'
 
+    def get_queryset(self):
+        return super(IndexListView, self).get_queryset().filter(user=self.request.user)
+
 
 class AlbumDetailView(generic.DetailView):
     model = Album
     template_name = 'music/album_detail.html'
+
+    # def get_queryset(self):
+    #     return super(IndexListView, self).get_queryset().filter(user=self.request.user)
 
 
 class SongDetailView(generic.DetailView):
     model = Song
     template_name = 'music/song_detail.html'
 
+    def get_queryset(self):
+        return super(IndexListView, self).get_queryset().filter(user=self.request.user)
+
 
 @method_decorator(login_required, name='dispatch')
 class AlbumCreateView(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
-    # success_url = reverse_lazy('music:index')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
 @method_decorator(login_required, name='dispatch')
-class AlbumUpdateView(UpdateView):
+class AlbumUpdateView(UserPassesTestMixin, UpdateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
-    success_url = reverse_lazy('music:index')
+
+    def test_func(self):
+        album = self.get_object()
+
+        if self.request.user == album.user:
+            return True
+    
+        return False
 
 
 @method_decorator(login_required, name='dispatch')
-class AlbumDeleteView(DeleteView):
+class AlbumDeleteView(UserPassesTestMixin, DeleteView):
     model = Album
     success_url = reverse_lazy('music:index')
+
+    def test_func(self):
+        album = self.get_object()
+
+        if self.request.user == album.user:
+            return True
+    
+        return False
 
 
 @method_decorator(login_required, name='dispatch')
@@ -55,13 +79,29 @@ class SongCreateView(CreateView):
 
 
 @method_decorator(login_required, name='dispatch')
-class SongUpdateView(UpdateView):
+class SongUpdateView(UserPassesTestMixin, UpdateView):
     model = Song
     fields = ['album', 'song_title', 'audio_file']
     success_url = reverse_lazy('music:index')
 
+    def test_func(self):
+        album = self.get_object()
+
+        if self.request.user == album.user:
+            return True
+    
+        return False
+
 
 @method_decorator(login_required, name='dispatch')
-class SongDeleteView(DeleteView):
+class SongDeleteView(UserPassesTestMixin, DeleteView):
     model = Song
     success_url = reverse_lazy('music:index')
+
+    def test_func(self):
+        album = self.get_object()
+
+        if self.request.user == album.user:
+            return True
+    
+        return False
